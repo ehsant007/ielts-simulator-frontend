63002eec5efe02a25b026a5d53a95c5cdd495e3e
Ehsan Tarakemeh <ehsansys@gmail.com>
2026-05-25 22:48:50 +0330

    fix lint errors

--- src/app/(site)/test/page.tsx
+++ src/app/(site)/test/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Button, VStack } from "@chakra-ui/react";
+import { Button, VStack } from "@chakra-ui/react";
 import { useSortable } from "@dnd-kit/react/sortable";
 
 
