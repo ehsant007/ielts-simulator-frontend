f763ee25603095dd62a514a4ddef56d38ec1ccdb
Ehsan Tarakemeh <ehsansys@gmail.com>
2026-06-17 19:27:18 +0330

    fix handling nested AdvText

--- src/components/lang-tools/AdvText.tsx
+++ src/components/lang-tools/AdvText.tsx
@@ -3,30 +3,41 @@
 import { Text as ChakraText, TextProps } from "@chakra-ui/react"
 import { HighlightSlice } from "./types"
 import { Highlighter } from "./Highlighter"
-import {tokenizer} from "./tokenizer"
+import { tokenizer } from "./tokenizer"
+import { createContext, useContext } from "react"
+
+
+const AdvTextContext = createContext<boolean>(false);
 
 type AdvTextProps = {
 	onWordPointed: (value: string) => void
 } & TextProps & HighlightSlice
 
 export function AdvText({ children, id, onWordPointed, highlights, setHighlights, ...props }: AdvTextProps) {
+	const insideAdvText = useContext(AdvTextContext)
+
 	let tokenIndex = 0
 	const nextTokenIndex = () => tokenIndex++
 
+	if (insideAdvText) {
+		return <ChakraText {...props}>{children}</ChakraText>
+	}
+
 	return (
-		<ChakraText {...props}>
-			{id ? (
-				<Highlighter
-					id={id}
-					highlights={highlights}
-					setHighlights={setHighlights}
-				>
-					{tokenizer(children, nextTokenIndex, onWordPointed)}
-				</Highlighter>
-			) : (
-				tokenizer(children, nextTokenIndex, onWordPointed)
-			)}
-		</ChakraText>
+		<AdvTextContext.Provider value={true}>
+			<ChakraText {...props}>
+				{id ? (
+					<Highlighter
+						id={id}
+						highlights={highlights}
+						setHighlights={setHighlights}
+					>
+						{tokenizer(children, nextTokenIndex, onWordPointed)}
+					</Highlighter>
+				) : (
+					tokenizer(children, nextTokenIndex, onWordPointed)
+				)}
+			</ChakraText>
+		</AdvTextContext.Provider>
 	)
 }
-
